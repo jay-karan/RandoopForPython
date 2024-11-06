f9d3670ec06f215a6b9438392916bd1843db5a95
Jay Karan Telukunta <jaykarantelukunta@gmail.com>
2024-11-05 23:36:15 -0500

    Reflection Implementation

---
+++ README.md
@@ -0,0 +1,2 @@
+## CS 591 - Software Testing 
+#### This is a course project repository for Randoop For Python.
---
+++ class_definitions.py
@@ -0,0 +1,15 @@
+# This is sample example class
+
+class SampleClass:
+    def add(self, a, b):
+        return a + b
+    
+    def multiply(self, a, b):
+        return a * b
+
+class AnotherClass:
+    def greet(self, name):
+        return f"Hello, {name}!"
+    
+    def farewell(self):
+        return "Goodbye!"
