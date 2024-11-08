27fa7113c6c765562a3d98cf5f0f689abdf250bc
Jay Vishaal J <jayvishaal17065@cse.ssn.edu.in>
2024-11-08 10:42:57 -0500

    Added CLI code and some test generation script

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="randoop-cli",
+    version="1.0",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "randoop-cli = randoop_cli.cli:main",
+        ]
+    },
+    install_requires=[],
+    author="Your Name",
+    description="Randoop-style test generator for Python classes",
+)
