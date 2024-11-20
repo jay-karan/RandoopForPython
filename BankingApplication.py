b35be7d9d73989e2b0332139f3daa4e1cf845b2a
Jay Vishaal J <jayvishaal17065@cse.ssn.edu.in>
2024-11-20 10:59:19 -0500

    Pushed Banking Change

--- BankingApplication.py
+++ BankingApplication.py
@@ -1,10 +1,17 @@
+import random
+class User:
+    def __init__(self) -> None:
+        self.user_name = random.choice(["ABdcd", "ihewoihrweio"])
+        self.acc_balance = random.randint(0, 101231223)
+
+        return
 class BankingApplication:
     def create_account(self, account_number: str, initial_balance: float = 0.0):
         return account_number, initial_balance
 
-    def deposit(self, account_number: str, current_balance: float, amount: float):
-        new_balance = current_balance + amount
-        return f"Deposited {amount} to account {account_number}. New balance: {new_balance}", new_balance
+    def deposit(self, user: User, amount: float):
+        new_balance = user.acc_balance + amount
+        return f"Deposited {amount} to account {user.user_name}. New balance: {new_balance}", new_balance
 
     def withdraw(self, account_number: str, current_balance: float, amount: float):
         if current_balance < amount:
